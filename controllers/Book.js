e7cf921578070e8c94f54b26a8e2f0a6b6ddf6a0
adhammhaydat <adhammohidat123@gmail.com>
2021-08-17 02:33:47 +0300

    solve lab12

---
+++ controllers/Book.js
@@ -0,0 +1,8 @@
+'use strict'
+const book=require('../modules/Book.modul')
+
+const bookController=(req,res)=>{
+
+  book.find( (err, book) => err ? res.send(err) : res.json(book))
+}
+module.exports=bookController;
