4b6018d56fe988b01d132b17c9cb5f9186ff8e8c
adhammhaydat <adhammohidat123@gmail.com>
2021-08-18 03:45:56 +0300

    to resolve later

--- controllers/Book.js
+++ controllers/Book.js
@@ -1,8 +1,40 @@
-'use strict'
-const book=require('../modules/Book.modul')
+"use strict";
+const book = require("../modules/Book.modul");
 
-const bookController=(req,res)=>{
+const bookController = (req, res) => {
+  book.find({}, (err, book) => (err ? res.send(err) : res.json(book)));
+};
+const handelUserInput = (req, res) => {
+  book.findOne({email:req.body.email},(err,bookContent)=>{
+    if(err){
+      res.send('Ooops we have a error')
+    }else{
+      let data ={
+        title:req.body["title"],
+        description:req.body["description"],
+        status:req.body["status"],
+        
+      };
+      bookContent.aboutBooks.push(data);
+      console.log(bookContent);
+      bookContent.save();
+      res.json(bookContent);
 
-  book.find({}, (err, book) => err ? res.send(err) : res.json(book))
-}
-module.exports=bookController;
+    }
+  })
+  ;
+};
+
+const deleteUser = (req, res) => {
+  let userId = req.parmas["id"];
+
+  book.findByIdAndDelete({ _id: userId }, (err, item) => {
+    if (err) {
+      console.log(oops);
+    } else {
+      console.log(userId);
+      res.send(userId);
+    }
+  });
+};
+module.exports = { bookController, handelUserInput, deleteUser };
--- server.js
+++ server.js
@@ -1,84 +1,89 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const jwksClient = require("jwks-rsa");
 const app = express();
-const PORT = process.env.PORT
-const Book=require('./modules/Book.modul')
+app.use(express.json());
+const PORT = process.env.PORT;
+const Book = require("./modules/Book.modul");
 app.use(cors());
-const mongoose=require('mongoose')
-const bookController=require('./controllers/Book');
+const mongoose = require("mongoose");
+const {
+  bookController,
+  handelUserInput,
+  deleteUser,
+} = require("./controllers/Book");
 
+mongoose.connect("mongodb://localhost:27017/Book", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect('mongodb://localhost:27017/Book', {useNewUrlParser: true,useUnifiedTopology: true});
-
-const seedBook=()=>{
-  const newBook=new Book({
-    email:"adhammohidat123@gmail.com",
-    aboutBooks:[{
-      description:"web developer",
-    status:"beginer",
-    title:"ADHAM MHAYDAT",},
-    {
-      description:"full stack",
-      status:"beginer",
-      title:"MA'MOON",
-      
-    },
-    {
-      description:"DESIGNER",
-      status:"beginer",
-      title:"THAA'ER",
-      
-    }]
-    
-    
-  })
+const seedBook = () => {
+  const newBook = new Book({
+    email: "adhammohidat123@gmail.com",
+    aboutBooks: [
+      {
+        description: "web developer",
+        status: "beginer",
+        title: "ADHAM MHAYDAT",
+      },
+      {
+        description: "full stack",
+        status: "beginer",
+        title: "MA'MOON",
+      },
+      {
+        description: "DESIGNER",
+        status: "beginer",
+        title: "THAA'ER",
+      },
+    ],
+  });
   newBook.save();
   console.log(newBook);
-}
-  // seedBook();
-  
-app.get('/book',bookController)
+};
+// seedBook();
 
+app.get("/book", bookController);
+
+app.post("/user-input", handelUserInput);
+
+app.delete("/user-delete/:id", deleteUser);
 
 const client = jwksClient({
-  // this url comes from your app on the auth0 dashboard 
-  jwksUri: `https://${process.env.DOMAIN}/.well-known/jwks.json`
+  // this url comes from your app on the auth0 dashboard
+  jwksUri: `https://${process.env.DOMAIN}/.well-known/jwks.json`,
 });
 
 // this is a ready to use function
-const getKey=(header, callback)=>{
-  client.getSigningKey(header.kid, function(err, key) {
-      const signingKey = key.publicKey || key.rsaPublicKey;
-      callback(null, signingKey);
-    });
-}
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+};
 // app.get('/book',(req,res)=>{
 
-  
 // });
 
-
-
-app.get('/auth', (request, response) => {
-  const token=request.headers.authorization.split(' ')[1];
-  jwt.verify(token,getKey,{},(err,user)=>{
-      if(err){
-        response.send('invalid token');
-      }
-      response.send(user)
-      console.log('hiiii')
-  })
-  // TODO: 
+app.get("/auth", (request, response) => {
+  const token = request.headers.authorization.split(" ")[1];
+  jwt.verify(token, getKey, {}, (err, user) => {
+    if (err) {
+      response.send("invalid token");
+    }
+    response.send(user);
+    console.log("hiiii");
+  });
+  // TODO:
   // STEP 1: get the jwt from the headers
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+});
 
-})
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`))
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
